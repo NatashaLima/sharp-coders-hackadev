14784a449d481d16d3a03753417ecdd1260e0a95
Natasha Lima <natasha.academicotsi@gmail.com>
2023-03-21 05:04:22 -0300

    :sparkles: feat: Implementando classes para estilizar a validação e dados
    Implementação de classes para estilizar a validação de dados do formulário de cadastro

--- Babie-Bank/js/modules/register.js
+++ Babie-Bank/js/modules/register.js
@@ -5,75 +5,98 @@ const phonenumberInput = document.getElementById('phonenumber');
 const emailInput = document.getElementById('email');
 const passwordInput = document.getElementById('password');
 const confirmPasswordInput = document.getElementById('confirmpassword');
-const newsletterCheckbox = document.getElementById('newsletter');
+const termCheckbox = document.getElementById('termCheckbox');
 
-// Função para validar o CPF
-function validateCPF() {
-  const cpf = cpfInput.value.trim();
-  if (cpf.length !== 11 || isNaN(cpf)) {
-    console.log('CPF inválido');
-    return false;
-  }
-  console.log('CPF válido');
-  return true;
+
+function setError(element, message, errorMessageElement){
+    element.classList.add('error');
+    const errorMessage = element.parentElement.querySelector('.error-message');
+    errorMessageElement.innerText = message;
+    errorMessageElement.style.display = 'block';
 }
+
+function clearError(element, errorMessageElement){
+    element.classList.remove('error');
+    const errorMessage = element.parentElement.querySelector('.error-message');
+    errorMessageElement.style.display = 'none';    
+}
+
+  // Função para validar o CPF
+function validateCPF() {
+    const cpf = cpfInput.value.trim();
+    const cpfErrorMessage = document.getElementById('cpf-error-message');
+    if (cpf.length !== 11 || isNaN(cpf)) {
+      console.log('CPF inválido');
+      setError(cpfInput, 'CPF inválido', cpfErrorMessage);
+      return false;
+    }
+    clearError(cpfInput, cpfErrorMessage);
+    return true;
+  }
 
 // Função para validar o nome
 function validateName() {
-  const name = usernameInput.value.trim();
-  if (name.length < 3) {
-    console.log('O nome deve ter no mínimo 3 caracteres');
-    return false;
+    const name = usernameInput.value.trim();
+    const errorMessageElement = document.getElementById('name-error-message');
+    if (name.length < 3) {
+      setError(usernameInput, 'O nome deve ter no mínimo 3 caracteres', errorMessageElement);
+      return false;
+    }
+    clearError(usernameInput, errorMessageElement);
+    return true;
   }
-  console.log('Nome válido');
-  return true;
-}
 
 // Função para validar o número de celular
 function validatePhoneNumber() {
-  const phoneNumber = phonenumberInput.value.trim();
-  if (phoneNumber.length !== 11 || isNaN(phoneNumber)) {
-    console.log('Número de celular inválido');
-    return false;
+    const phoneNumber = phonenumberInput.value.trim();
+    const errorMessageElement = document.getElementById('phonenumber-error-message');
+    if (phoneNumber.length !== 11 || isNaN(phoneNumber)) {
+      setError(phonenumberInput, 'Número de celular inválido', errorMessageElement);
+      return false;
+    }
+    clearError(phonenumberInput, errorMessageElement);
+    return true;
   }
-  console.log('Número de celular válido');
-  return true;
-}
 
 // Função para validar o email
 function validateEmail() {
-  const email = emailInput.value.trim();
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!emailRegex.test(email)) {
-    console.log('Email inválido');
-    return false;
+    const email = emailInput.value.trim();
+    const errorMessageElement = document.getElementById('email-error-message');
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      setError(emailInput, 'Email inválido', errorMessageElement);
+      return false;
+    }
+    clearError(emailInput, errorMessageElement);
+    return true;
   }
-  console.log('Email válido');
-  return true;
-}
 
 // Função para validar a senha
 function validatePassword() {
-  const password = passwordInput.value.trim();
-  if (password.length < 8) {
-    console.log('A senha deve ter no mínimo 8 caracteres');
-    return false;
+    const password = passwordInput.value.trim();
+    const errorMessageElement = document.getElementById('password-error-message');
+    if (password.length < 8) {
+      setError(passwordInput, 'A senha deve ter no mínimo 8 caracteres', errorMessageElement);
+      return false;
+    }
+    clearError(passwordInput, errorMessageElement);
+    return true;
   }
-  console.log('Senha válida');
-  return true;
-}
 
 // Função para validar a confirmação da senha
 function validateConfirmPassword() {
-  const password = passwordInput.value.trim();
-  const confirmPassword = confirmPasswordInput.value.trim();
-  if (password !== confirmPassword) {
-    console.log('As senhas não coincidem');
-    return false;
+    const password = passwordInput.value.trim();
+    const confirmPassword = confirmPasswordInput.value.trim();
+    const errorMessageElement = document.getElementById('confirmpassword-error-message');
+    if (password !== confirmPassword) {
+      setError(confirmPasswordInput, 'As senhas não coincidem', errorMessageElement);
+      return false;
+    }
+    clearError(confirmPasswordInput, errorMessageElement);
+    return true;
   }
-  console.log('Confirmação de senha válida');
-  return true;
-}
+
+  
 
 // Função para validar o formulário como um todo
 function validateForm() {
@@ -102,10 +125,11 @@ submitButton.addEventListener('click', (event) => {
 //Validar os termos e condições do formulário
 function validateForm() {
   
-    const termCheckbox = document.getElementById("term");
+    const termCheckbox = document.getElementById("termCheckbox");
     if (!termCheckbox.checked) {
       alert("Você precisa aceitar os termos para continuar.");
       return false;
     }  
   }
-  
+
+ 
