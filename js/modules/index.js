8cb2d7b21d446b0a279263669f6ba2ff28720309
Lucas Ferreira <lks_ferreira@hotmail.com>
2023-03-21 14:56:03 -0300

    :sparkles: feat: Adicionado funcionalidade ao botões
    - Implementado código javascrip para redirecionamento das páginas de acesso e cadastro.

--- js/modules/index.js
+++ js/modules/index.js
@@ -1,3 +1,10 @@
 const botaoAcessar = document.getElementById("botao-acessar");
+const botaoCadastrar = document.getElementById("botao-cadastrar");
 
-console.log(botaoAcessar);
+botaoAcessar.addEventListener('click', function() {
+   window.location.href = ("view/login.html");
+});
+
+botaoCadastrar.addEventListener('click', function(){
+    window.location.href = ("view/register.html");
+});
