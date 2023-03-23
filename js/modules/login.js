2c6cd3b475d27b5cf7a852cb5f3b75bcf0fabf64
Lucas Ferreira <lks_ferreira@hotmail.com>
2023-03-23 11:52:13 -0300

    :recycle: refactor: Lógica tela de login
    - Refatorado a lógica da tela de login.

--- js/modules/login.js
+++ js/modules/login.js
@@ -1,51 +1,21 @@
-    // deu trabalho, mas consegui. Preciso que vc faça uma alteração em registro.
-    // Quando vcs registram algo, o registro está sobreescrevendo o outro
-    // armazena esses registros em um uma lista de usuarios tipo 
-    // listaUsarios [] depois passa os dados com listaUsarios.push()
-    // assim teremos uma estrutura com varias outras estruturas dentro. 
-    // fica mapeado e mais facil de trabalhar
+// deu trabalho, mas consegui. Preciso que vc faça uma alteração em registro.
+// Quando vcs registram algo, o registro está sobreescrevendo o outro
+// armazena esses registros em um uma lista de usuarios tipo 
+// listaUsarios [] depois passa os dados com listaUsarios.push()
+// assim teremos uma estrutura com varias outras estruturas dentro. 
+// fica mapeado e mais facil de trabalhar
 
+const cpf = document.getElementById("cpf-login");
+const password = document.getElementById("password-login");
+const formDataJson = JSON.parse(localStorage.getItem("formData"));
 
+const botaoLogin = document.getElementById("botao-login");
+botaoLogin.addEventListener('click', entrar);
 
 function entrar() {
-    const cpf = document.getElementById("cpf-login").value;
-    const senha = document.getElementById("password-login").value;
-
-    formDataJson = JSON.parse(localStorage.getItem("formData"));
-    alert("passa 1");
-
-    const formDataArray = [];
-
-    for (const key in formDataJson) {
-        if (formDataJson.hasOwnProperty(key)) {
-            formDataArray.push(formDataJson[key]);
-        }
-    }
-
-
-    alert("passa 2");
-    formDataArray.forEach((item) => {
-        if(cpf.value == item.cpf && senha.value == item.password){
-           
-          userValid = {
-             cpf: item.cpf,
-             password: item.senhaCad
-           }
-          
-        }
-      })
-       
-    alert("passa 3");
-    if (cpf.value == userValid.cpf && senha.value == userValid.cpf) {
+    if (formDataJson.cpf == cpf.value && formDataJson.password == password.value) {
         window.location.href = "https://github.com/LKSFerreira/sharp-coders-hackadev";
     } else {
-      
-      alert("CPF ou senha inválidos");
+        alert('CPF ou Senha incorretos, tente novamente');
     }
 }
- 
-
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("botao-login").addEventListener("click", entrar);
-});
-
